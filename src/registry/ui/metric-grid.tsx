bd14aee6d7532144d2856670adb2dd32cf1f6e41
Bruno Petrovic <224663123+brunopetrovic@users.noreply.github.com>
2026-08-08 14:39:33 +0200

    add metric grid signature component

---
+++ src/registry/ui/metric-grid.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function MetricGrid({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("grid gap-3 sm:grid-cols-2 lg:grid-cols-4", className)} {...props} />;
+}
